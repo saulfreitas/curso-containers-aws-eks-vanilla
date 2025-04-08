98cd5347a038a26f0baba6c38c5e0f34b84d0994
Saul Freitas <saulfreitas@gmail.com>
2025-04-08 16:57:45 -0300

    chore: update file data.tf

--- data.tf
+++ data.tf
@@ -7,11 +7,6 @@ data "aws_ssm_parameter" "public_subnets" {
   name  = var.ssm_public_subnets[count.index]
 }
 
-data "aws_ssm_parameter" "public_subnets" {
-  count = length(var.ssm_public_subnets)
-  name  = var.ssm_public_subnets[count.index]
-}
-
 data "aws_ssm_parameter" "private_subnets" {
   count = length(var.ssm_private_subnets)
   name  = var.ssm_private_subnets[count.index]
